36d16771fa3925f1928c1299b92c479cb9d744e4
Muhammad Talha <m.talhase@gmail.com>
2022-08-09 05:22:59 +0500

    feat: add cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express();
+const cors = require('cors')
 const bodyParser = require("body-parser");
 const connectDB = require('./config/db');
 const sampleRoute=require('./routes/index')
+
+app.use(cors())
 app.use(bodyParser.json());
 //Connecting Database
 connectDB();
